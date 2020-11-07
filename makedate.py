0655809922152a44229e5782694cf931440ff0cd
Darius <67561957+Radius-coder@users.noreply.github.com>
2020-11-07 11:36:07 +0000

    Delete makedate.py

--- makedate.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import os
-from gtts import gTTS
-#get today's date
-date = datetime.datetime.now()
-x = date.strftime("%d"+" %B, %Y")
-tts = gTTS(text=x, lang='en')
-tts.save("date.mp3")
-print("Audio loaded!")
